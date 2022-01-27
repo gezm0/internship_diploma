1e77ab621d38d19c2015ae66daa991d3fd3e1985
gezm0 <94368360+gezm0@users.noreply.github.com>
2022-01-27 21:39:19 +0300

    Add files via upload

---
+++ pull_data.py
@@ -0,0 +1,48 @@
+# for work with database
+import psycopg2
+# for work with site through API
+import requests
+
+# request data through AIP section
+
+# where to get data
+data_people = f"https://swapi.dev/api/people/"
+data_starships = f"https://swapi.dev/api/starships/"
+
+# work with data about people
+def get_raw_sw_data_people():
+    resp = requests.get(data_people)
+    if resp.status_code == 200:
+        sw_data_people = resp.json()
+    else:
+        print('We have some issues')
+    return sw_data_people
+print(get_raw_sw_data_people())
+
+# work with data about starships
+def get_raw_sw_data_starships():
+    resp = requests.get(data_starships)
+    if resp.status_code == 200:
+        sw_data_starships = resp.json()
+    else:
+        print('We have some issues')
+    return sw_data_starships
+print(get_raw_sw_data_starships())
+
+
+# database section
+# define connection method
+#conn = psycopg2.connect(host='127.0.0.1', 
+#                        port="5432", 
+#                        dbname='test', 
+#                        user='test', 
+#                        password='test')
+
+# checking the connection status and displaying the result
+#if conn:
+#        print('All OK')
+#else:
+#        print('Something wrong with connection')
+#        breakpoint
+
+#cursor = conn.cursor()                  # need for working with database
