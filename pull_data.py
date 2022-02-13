36e2c0c150fe85b0c405f7173e4953777f4c4c3b
gezm0 <eremin@ryazan.net.ru>
2022-02-13 20:03:25 +0300

    api result parsing for persons and starships

--- pull_data.py
+++ pull_data.py
@@ -5,17 +5,30 @@ import requests
 # request data through AIP section
 
 person_num = 1
+person_num_last = 90
+starship_num = 1
+starship_num_last = 50
+
 data_people = f"https://swapi.dev/api/people/"
-data_people_new = ''
-people = '[]'
+data_starships = f"https://swapi.dev/api/starships/"
 
 def get_raw_people():
+    people = ''
     resp = requests.get(data_people_new)
     if resp.status_code == 200:
         people = resp.json()
     else:
-        print('We have some issues')
+        print('None')
     return people
+
+def get_raw_starships():
+    starships = ''
+    resp = requests.get(data_starships_new)
+    if resp.status_code == 200:
+        starships = resp.json()
+    else:
+        print('')
+    return starships
 
 def pre_people(raw_data):
     people_name = raw_data['name']
@@ -24,25 +37,38 @@ def pre_people(raw_data):
     people_starships = raw_data['starships']
     return people_name, people_gender, people_homeworld, people_starships
 
-while person_num <= 50:
-    data_people_new = data_people + str(person_num)
-    person = pre_people(get_raw_people())
-    p_name = person[0]
-    p_gender = person[1]
-    p_homeworld = person[2]
-    p_starships = person[3]
-    print(p_name, p_gender, p_homeworld, p_starships)
+while person_num <= person_num_last:
+    try:
+        data_people_new = data_people + str(person_num)
+        person = pre_people(get_raw_people())
+        p_name = person[0]
+        p_gender = person[1]
+        p_homeworld = person[2]
+        p_starships = person[3]
+        print(p_name, p_gender, p_homeworld, p_starships)
+    except:
+        pass
     person_num = person_num + 1
 
-# work with data about starships
-#def get_raw_sw_data_starships():
-#    resp = requests.get(data_starships)
-#    if resp.status_code == 200:
-#        sw_data_starships = resp.json()
-#    else:
-#        print('We have some issues')
-#    return sw_data_starships
-#print(get_raw_sw_data_starships())
+def pre_starships(raw_data):
+    starship_name = raw_data['name']
+    starship_model = raw_data['model']
+    starship_manufacturer = raw_data['manufacturer']
+    starship_cargo_capacity = raw_data['cargo_capacity']
+    return starship_name, starship_model, starship_manufacturer, starship_cargo_capacity
+
+while starship_num <= starship_num_last:
+    try:
+        data_starships_new = data_starships + str(starship_num)
+        starship = pre_starships(get_raw_starships())
+        s_name = starship[0]
+        s_model = starship[1]
+        s_manufacturer = starship[2]
+        s_cargo_capacity = starship[3]
+        print(s_name, s_model, s_manufacturer, s_cargo_capacity)
+    except:
+        pass
+    starship_num = starship_num + 1
 
 # todo
 # work with raw data
