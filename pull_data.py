3c8cf3012f04ab8160e25a3cf2c5d6bbf406a5fc
gezm0 <eremin@ryazan.net.ru>
2022-02-13 18:05:44 +0300

    api result parsing

--- pull_data.py
+++ pull_data.py
@@ -1,33 +1,48 @@
-# for work with database
+from unicodedata import name
 import psycopg2
-# for work with site through API
 import requests
 
 # request data through AIP section
 
-# from where get data
+person_num = 1
 data_people = f"https://swapi.dev/api/people/"
-data_starships = f"https://swapi.dev/api/starships/"
+data_people_new = ''
+people = '[]'
 
-# work with data about people
-def get_raw_sw_data_people():
-    resp = requests.get(data_people)
+def get_raw_people():
+    resp = requests.get(data_people_new)
     if resp.status_code == 200:
-        sw_data_people = resp.json()
+        people = resp.json()
     else:
         print('We have some issues')
-    return sw_data_people
-print(get_raw_sw_data_people())
+    return people
+
+def pre_people(raw_data):
+    people_name = raw_data['name']
+    people_gender = raw_data['gender']
+    people_homeworld = raw_data['homeworld']
+    people_starships = raw_data['starships']
+    return people_name, people_gender, people_homeworld, people_starships
+
+while person_num <= 50:
+    data_people_new = data_people + str(person_num)
+    person = pre_people(get_raw_people())
+    p_name = person[0]
+    p_gender = person[1]
+    p_homeworld = person[2]
+    p_starships = person[3]
+    print(p_name, p_gender, p_homeworld, p_starships)
+    person_num = person_num + 1
 
 # work with data about starships
-def get_raw_sw_data_starships():
-    resp = requests.get(data_starships)
-    if resp.status_code == 200:
-        sw_data_starships = resp.json()
-    else:
-        print('We have some issues')
-    return sw_data_starships
-print(get_raw_sw_data_starships())
+#def get_raw_sw_data_starships():
+#    resp = requests.get(data_starships)
+#    if resp.status_code == 200:
+#        sw_data_starships = resp.json()
+#    else:
+#        print('We have some issues')
+#    return sw_data_starships
+#print(get_raw_sw_data_starships())
 
 # todo
 # work with raw data
