7c2727164f7e71c57c39def0fac0e39c543be02c
gezm0 <eremin@ryazan.net.ru>
2022-02-14 18:50:16 +0300

    just for study

--- study.py
+++ study.py
@@ -1,12 +1,25 @@
 import turtle
 
+def david():
+    for step in range(6):
+        turtle.begin_fill()
+        for i in range(3):
+            turtle.forward(50)
+            turtle.left(360 / 3)
+        turtle.end_fill()
+        
+        turtle.forward(50)
+        turtle.right(60)
+
 turtle.shape('turtle')
 turtle.shapesize(5)
-turtle.color('red')
-turtle.speed(1)
+turtle.color('black', 'blue')
+turtle.speed(10)
 
-for step in range(6):
-    turtle.forward(50)
-    turtle.right(60)
+david()
+
+turtle.backward(200)
+
+david()
 
 turtle.hideturtle()
